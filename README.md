5e0423143020c42f27ae180df6d33baacf7e7764
“raghu” <v.raghupavan@gmail.com>
2016-09-07 23:47:10 -0500

    adding assignment2 from local

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # assignment2
+find the screen shots in the wiki link here https://github.com/raghupavan/assignment2/wiki/lab2-screenshots
---
+++ Source/untitled/.idea/login.js
@@ -0,0 +1,6 @@
+/**
+ * Created by rAj on 9/5/2016.
+ */
+$('.message a').click(function(){
+    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+});
